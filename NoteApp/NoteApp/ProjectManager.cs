8c6e7da0bac70c20336a3aa466a1e3a7baf3a5c3
Василий В. Захаров <5851_zvv@kcup.net>
2019-01-13 23:56:25 +0700

    Добавлены неотслеживаемые файлы

---
+++ NoteApp/NoteApp/ProjectManager.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace NoteApp
+{
+    public static class ProjectManager
+    {
+        /// <summary>
+        /// Экземпляр сериализатора.
+        /// </summary>
+        private static readonly JsonSerializer _serializer = new JsonSerializer();
+
+        /// <summary>
+        /// Сериализирование проекта в строку и сохранение в файл.
+        /// </summary>
+        /// <param name="saveProject"> Сохраняемый проект. </param>
+        public static void SaveProject(Project saveProject, string streamWriter)
+        {
+            using (StreamWriter sw = new StreamWriter(streamWriter))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                _serializer.Serialize(writer, saveProject);
+            }
+
+        }
+
+        /// <summary>
+        /// Считывание json текста из файла и десериализация в проект.
+        /// </summary>
+        public static Project OpenProject(string streamReader)
+        {
+            if (!File.Exists(streamReader))
+            {
+                return null;
+            }
+
+            using (StreamReader sr = new StreamReader(streamReader))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                return _serializer.Deserialize<Project>(reader);
+            }
+        }
+    }
+}
---
+++ NoteApp/NoteAppUI/AddNoteForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NoteApp;
+
+namespace NoteAppUI
+{
+    public partial class AddNoteForm : Form
+    {
+        /// <summary>
+        /// Поле для передачи данных.
+        /// </summary>
+        private Note _data;
+
+        /// <summary>
+        /// Определение поля _data.
+        /// </summary>
+        public Note Data
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                _data = value;
+
+                if (_data != null)
+                {
+                    NewTextTextBox.Text = _data.Text;
+                    NewTitleTextBox.Text = _data.Title;
+                    NewTimeCreateLabel.Text = _data.TimeCreation.ToString("dd/MM/yyyy");
+                    NewTimeModifiedLabel.Text = DateTime.Now.ToString("dd/MM/yyyy");
+                }
+            }
+        }
+
+        public AddNoteForm()
+        {
+            InitializeComponent();
+
+            AddCategoryBox();
+        }
+
+        /// <summary>
+        /// Кнопка для возвращение на главную форму
+        /// </summary>
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _data.Title = NewTitleTextBox.Text;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                _data.Text = NewTextTextBox.Text;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                _data.TimeModified = DateTime.Now;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                     MessageBoxIcon.Error);
+                return;
+            }
+
+            _data.Category = EnumParser.StringToNoteCategory(NewNoteCategoryBox.Text);
+
+            this.DialogResult = DialogResult.OK;
+        }
+
+        /// <summary>
+        /// Метод для добавления категорий
+        /// </summary>
+        private void AddCategoryBox()
+        {
+            NewNoteCategoryBox.DataSource = Enum.GetValues(typeof(NoteCategory));
+            NewNoteCategoryBox.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Кнопка отмены (выход из окна добавления/изменения без сохранения)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void NewTitleText_TextChanged(object sender, EventArgs e)
+        {
+            if (NewTitleTextBox.Text.Length > 50)
+            {
+                NewTitleTextBox.ForeColor = Color.Red;
+            }
+            else
+            {
+                NewTitleTextBox.ForeColor = Color.Black;
+            }
+        }
+    }
+}
